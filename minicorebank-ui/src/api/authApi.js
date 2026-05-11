ac829003a1cad3275d6837b390aedb6d4ecdfa91
Jamkhande Dattatray <84066284+Datta8517@users.noreply.github.com>
2026-05-12 00:36:04 +0530

    Update authApi.js

--- minicorebank-ui/src/api/authApi.js
+++ minicorebank-ui/src/api/authApi.js
@@ -1,19 +1,19 @@
 import api from "./axios";
 
 export const login = (data) =>
-  api.post("/api/auth/login", data);
+  api.post("/auth/login", data);
 
 export const register = (data) =>
-  api.post("/api/auth/register", data);
+  api.post("/auth/register", data);
 
 export const logout = () => {
   const refreshToken = localStorage.getItem("refreshToken");
 
-  return api.post("/api/auth/logout", {
+  return api.post("/auth/logout", {
     refreshToken
   });
 };
 
 
 export const twoFactorAuth =(data) =>
-    api.post("/api/auth/2fa/setup")
+    api.post("/auth/2fa/setup")
