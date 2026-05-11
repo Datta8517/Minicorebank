5ef9989c1aceab0bfbbc5fc2eaeec2cd3c37715a
Jamkhande Dattatray <84066284+Datta8517@users.noreply.github.com>
2026-05-12 00:36:41 +0530

    Update Login.jsx

--- minicorebank-ui/src/auth/Login.jsx
+++ minicorebank-ui/src/auth/Login.jsx
@@ -10,7 +10,7 @@ export default function Login() {
 
   const handleLogin = async () => {
     try {
-      const res = await api.post("/api/auth/login", {
+      const res = await api.post("/auth/login", {
         username,
         password,
         pin: pin || null
