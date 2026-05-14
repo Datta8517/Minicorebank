6a3e2d9d19c1fa4ea66493bc5755a0931510fc0d
Ubuntu <ubuntu@ip-172-31-17-105.eu-north-1.compute.internal>
2026-05-14 19:40:21 +0000

    UI deployment update first

--- minicorebank-ui/src/api/axios.js
+++ minicorebank-ui/src/api/axios.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
 const api = axios.create({
+<<<<<<< HEAD
   baseURL: import.meta.env.VITE_API_URL
+=======
+  baseURL:import.meta.env.VITE_API_URL
+>>>>>>> f52044e (UI deployment update first)
 });
 
 // attach access token
